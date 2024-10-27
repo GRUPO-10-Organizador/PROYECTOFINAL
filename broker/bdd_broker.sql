f96a75b76562a9d86aba241e23f1817d53085c96
Agustin <agustin.97.ceballos@gmail.com>
2024-10-27 02:01:35 -0300

    add table empresa

--- broker/bdd_broker.sql
+++ broker/bdd_broker.sql
@@ -9,3 +9,12 @@ CREATE TABLE Usuario (
     email VARCHAR(100) NOT NULL UNIQUE,
     saldo DECIMAL(15, 2) DEFAULT 0.00
 );
+
+
+CREATE TABLE Empresa (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    sector VARCHAR(50),
+    pais VARCHAR(50)
+);
+
