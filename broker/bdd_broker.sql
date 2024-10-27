374a1d18f74688d9945a474d1073db34ed119b00
NachiitoPlay <NachiitoPlayYT@gmail.com>
2024-10-27 01:53:20 -0300

    add Table Usuario

--- broker/bdd_broker.sql
+++ broker/bdd_broker.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE broker;
 USE broker;
 
+#tabla usuario
+
+CREATE TABLE Usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    saldo DECIMAL(15, 2) DEFAULT 0.00
+);
