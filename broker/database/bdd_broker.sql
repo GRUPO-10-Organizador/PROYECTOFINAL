ad5409aa96dfe20c078350a492e3d0755d7da830
NachiitoPlay <NachiitoPlayYT@gmail.com>
2024-10-27 21:02:31 -0300

    Add broker

--- broker/database/bdd_broker.sql
+++ broker/database/bdd_broker.sql
@@ -1,15 +1,12 @@
 CREATE DATABASE broker;
 USE broker;
 
-#tabla usuario
-
 CREATE TABLE Usuario (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     saldo DECIMAL(15, 2) DEFAULT 0.00
 );
-
 
 CREATE TABLE Empresa (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -47,5 +44,3 @@ CREATE TABLE Portafolio (
     FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
     FOREIGN KEY (accion_id) REFERENCES Accion(id)
 );
-
-
